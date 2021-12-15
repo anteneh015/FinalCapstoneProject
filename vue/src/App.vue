235a370dafada5e140d997c9a123e608e09df82c
Liping Castillo <lipingzdaisy@gmail.com>
2021-12-14 20:25:46 -0500

    worked!

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -23,11 +23,11 @@ export default {
       return this.$store.state.user != "currentUser";
     },
   },
-    created() {
+created() {
   attendeeService
     .getAllAttendees()
     .then((response) => {
-      this.filteredAttendees = response.data;
+       this.$store.commit("SET_ATTENDEES", response.data);
     })
     .catch((error) => {
       if (error.response) {
@@ -49,7 +49,7 @@ export default {
 </script>
 
 <style scoped>
-html {
+/* html {
     height: 100%; 
     width: 100%;
     position: relative;
@@ -57,5 +57,5 @@ html {
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
-}
+} */
 </style>
