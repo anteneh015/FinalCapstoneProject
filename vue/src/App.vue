897afbb33e87cabe3ef2ef15c855c4fa822b2390
Liping Castillo <lipingzdaisy@gmail.com>
2021-12-14 20:27:30 -0500

    worked

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -23,7 +23,7 @@ export default {
       return this.$store.state.user != "currentUser";
     },
   },
-    created() {
+  created() {
   attendeeService
     .getAllAttendees()
     .then((response) => {
